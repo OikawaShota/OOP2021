57c829eb8c606dcea7c5bacad3a0e0631d3e9540
Oikawashota <32017@ojs.ac.jp>
2021-05-27 15:51:23 +0900

    P70 リスト2.22

--- SalesCalculator/SalesCounter.cs
+++ SalesCalculator/SalesCounter.cs
@@ -1,17 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SalesCalculator {
 //売上集計クラス
-    class SalesCounter {
+     class SalesCounter {
         private List<Sale> _sales;
 
-        public SalesCounter(List<Sale> sales) {
-            _sales = sales;
+        public SalesCounter(string filePath) {
+            _sales = ReadSales(filePath);
         }
+
+
+        //売上データを読み込み、Saleオブジェクトのリストを返す
+        private static List<Sale> ReadSales(string filePath) {
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach(string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
+        }
+
+
         public Dictionary<string, int> GetperStoreSales() {
             Dictionary<string, int> dict = new Dictionary<string, int>();
             foreach(Sale sale in _sales) {
