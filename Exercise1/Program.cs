62af585b7af8431ea6afaf3fe8fed6d415f7ed45
Oikawashota <32017@ojs.ac.jp>
2021-06-01 11:20:54 +0900

    P79 演習問題1

--- Exercise1/Program.cs
+++ Exercise1/Program.cs
@@ -6,8 +6,19 @@ using System.Threading.Tasks;
 
 namespace Exercise1 {
     class Program {
-        static void Main(string[] args) {
+        private static void Main(string[] args) {
+            var songs = new Song[] {
+            new Song("a", "AAA", 240),
+            new Song("b", "BBB", 190),
+            new Song("c", "CCC", 200)
+        };
+            PrintSongs(songs);
+        }
+        private static void PrintSongs(Song[] songs) {
+            foreach(var song in songs) {
+                Console.WriteLine(@"{0},{1},{2:m\:ss}",song.Title,song.ArtistName,TimeSpan.FromSeconds(song.Length));
 
+            }
         }
     }
 }
