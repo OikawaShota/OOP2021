f7fb4d2fad6d39350817f573179f16befea997e6
Oikawashota <32017@ojs.ac.jp>
2021-06-01 11:28:35 +0900

    P79 演習問題3

---
+++ Exercise3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    class Program {
+        static void Main(string[] args) {
+            var sales = new SalesCounter("Sales.csv");
+
+            var amountPerCategory = sales.GetPerCategorySales();//商品カテゴリ別売上を求める
+            foreach(var obj in amountPerCategory) {
+                Console.WriteLine("{0} {1:#,#}円", obj.Key, obj.Value);
+            }
+        }
+    }
+}
