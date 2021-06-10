255dde03272f8555debee56daae7b48112f0adbd
Oikawashota <32017@ojs.ac.jp>
2021-06-10 15:41:54 +0900

    P133 演習問題　終了時点

--- Chapter04/Section01/Program.cs
+++ Chapter04/Section01/Program.cs
@@ -7,6 +7,24 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
+            var Session = new Dictionary<string, object>();
+            Session["MyProduct"] = new Product();
+            var product = Session["MyProduct"] as Product;
+            if(product.Name == "") {
+                Console.WriteLine("productが取得できなかった");
+            } else {
+                Console.WriteLine("productが取得できた");
+            }
         }
+        private static Product GetProductA() {
+            Sale sale = new Sale();
+            return sale?.Product;
+        }
+
+    }
+    class Sale {
+        public string ShopNmae { get; set; } = "abcde";
+        public int Amount { get; set; } = 12340;
+        public Product Product { get; set; }
     }
 }
