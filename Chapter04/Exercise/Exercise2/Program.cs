a136f58a3b952b281d395b7bd9ca4d436d952199
Oikawashota <32017@ojs.ac.jp>
2021-06-11 15:53:13 +0900

    P158 演習問題5_1

--- Chapter04/Exercise/Exercise2/Program.cs
+++ Chapter04/Exercise/Exercise2/Program.cs
@@ -9,8 +9,50 @@ namespace Exercise2 {
     class Program {
         static void Main(string[] args) {
             //var ym = new YearMonth();
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980,1),
+                new YearMonth(1990,4),
+                new YearMonth(2000,7),
+                new YearMonth(2010,9),
+                new YearMonth(2020,12),
+            };
+            Exercise2_2(ymCollection);
+            Console.WriteLine("---");
 
+            Exercise2_4(ymCollection);
+            Console.WriteLine("---");
+
+            Exercise2_5(ymCollection);
+            Console.WriteLine("---");
         }
 
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+            foreach(var ym in ymCollection) {
+                Console.WriteLine(ym);
+            }
+        }
+
+        static YearMonth FindFirst21C(YearMonth[] ymCollection) {
+            bool a;
+            foreach(var b in ymCollection) {
+                if(b.Is21Century) {
+                    return b;
+                }
+            }
+            return null;
+        }
+
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+            var yearmonth=FindFirst21C(ymCollection); 
+            var s = yearmonth != null ? yearmonth.Year.ToString() : "21世紀のデータはありません";
+            Console.WriteLine(s);
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+            var array =ymCollection.Select(ym => ym.AddOneMonth()).ToArray();
+            foreach(var ym in array) {
+                Console.WriteLine(ym);
+            }
+        }
     }
 }
