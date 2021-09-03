0d3b0604d4f4552b830988e9ebb1327f0c8181f2
晶大 及川 <32017@ojs.ac.jp>
2021-09-03 15:42:07 +0900

    演習2完成

---
+++ WetherApp/WetherApp/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace WetherApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb1 = new System.Windows.Forms.ComboBox();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.tb1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(58, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "地域選択";
+            // 
+            // cb1
+            // 
+            this.cb1.Font = new System.Drawing.Font("MS UI Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.cb1.FormattingEnabled = true;
+            this.cb1.Items.AddRange(new object[] {
+            "前橋",
+            "みなかみ",
+            "宇都宮",
+            "水戸",
+            "その他"});
+            this.cb1.Location = new System.Drawing.Point(244, 20);
+            this.cb1.Name = "cb1";
+            this.cb1.Size = new System.Drawing.Size(137, 35);
+            this.cb1.TabIndex = 1;
+            // 
+            // bt1
+            // 
+            this.bt1.Location = new System.Drawing.Point(475, 34);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(128, 69);
+            this.bt1.TabIndex = 2;
+            this.bt1.Text = "天候表示";
+            this.bt1.UseVisualStyleBackColor = true;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // tb1
+            // 
+            this.tb1.Font = new System.Drawing.Font("MS UI Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tb1.Location = new System.Drawing.Point(48, 118);
+            this.tb1.Multiline = true;
+            this.tb1.Name = "tb1";
+            this.tb1.Size = new System.Drawing.Size(724, 381);
+            this.tb1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(43, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 28);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "コード入力用";
+            // 
+            // tb2
+            // 
+            this.tb2.Font = new System.Drawing.Font("MS UI Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.tb2.Location = new System.Drawing.Point(244, 69);
+            this.tb2.Name = "tb2";
+            this.tb2.Size = new System.Drawing.Size(137, 34);
+            this.tb2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 511);
+            this.Controls.Add(this.tb2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tb1);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.cb1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cb1;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.TextBox tb1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb2;
+    }
+}
+
---
+++ WetherApp/WetherApp/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WetherApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            string[] city = new string[5] { "前橋", "みなかみ", "宇都宮", "水戸" ,"その他"};
+            int[] citycode;
+            int a = 0;
+            if (cb1.Text.Equals("その他"))
+            {
+                citycode = new int[5] { 4210, 4220, 4110, 4010, int.Parse(tb2.Text) };
+                a = 4;
+            }
+            else
+            {
+                citycode = new int[4] { 4210, 4220, 4110, 4010};
+                int i = 0;
+                for (i = 0; i < city.Length; i++)
+                {
+                    if (cb1.Text.Equals(city[i]))
+                    {
+                        a = i;
+                        break;
+                    }
+                }
+            }
+            int code = citycode[a];
+            var results = GetWeatherReportFromYahoo(code);
+            foreach (var s in results)
+            {
+                tb1.Text += s.ToString();
+                tb1.AppendText(Environment.NewLine);
+            }
+        }
+
+        private IEnumerable<object> GetWeatherReportFromYahoo(int code)
+        {
+            using (var wc = new WebClient())
+            {
+                wc.Headers.Add("Content-type", "charset=UTF-8");
+                var uriString = string.Format(
+                    @"http://rss.weather.yahoo.co.jp/rss/days/{0}.xml", code);
+                var url = new Uri(uriString);
+                var stream = wc.OpenRead(url);
+
+                XDocument xdoc = XDocument.Load(stream);
+                var nodes = xdoc.Root.Descendants("title");
+                foreach (var node in nodes)
+                {
+                    string s = Regex.Replace(node.Value, "【|】| - Yahoo!天気・災害", "");
+                    yield return s;
+                }
+            }
+        }
+    }
+}
