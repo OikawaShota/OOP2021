b44dc88fd590681e99b068b4332d14e6ec06e126
晶大 及川 <32017@ojs.ac.jp>
2021-11-17 16:01:21 +0900

    NumberGame完成

--- WPF/NumberGame/MainWindow.xaml.cs
+++ WPF/NumberGame/MainWindow.xaml.cs
@@ -21,21 +21,62 @@ namespace NumberGame
     public partial class MainWindow : Window
     {
         static Random ans = new Random();
-        int a = int.Parse(ans.Next(1, 25).ToString());
+        private int a = ans.Next(Rows * Columns) + 1;
+        private const int Rows = 5;
+        private const int Columns = 5;
+
+        private SolidColorBrush selectedButtonColor = new SolidColorBrush(Colors.Yellow);
+        private SolidColorBrush hit = new SolidColorBrush(Colors.Red);
         public MainWindow()
         {
             InitializeComponent();
+
         }
+
+        private void Loded(object sender, RoutedEventArgs e)
+        {
+            List<Button> buttons = new List<Button>();
+
+            for (int i = 0; i < Rows; i++)
+            {
+                grid.RowDefinitions.Add(new RowDefinition());
+            }
+
+            for (int j = 0; j < Columns; j++)
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            int v = 0;
+            for (int i = 0; i < Rows; i++)
+            {
+                for (int j = 0; j < Columns; j++) {
+                    var bt = new Button();
+                    bt.Width = MainWind.Width / Columns;
+                    bt.Height = MainWind.Height/Rows;
+                    bt.Content = v + 1;
+                    bt.FontSize = 30;
+                    bt.Click += bt_Click;
+                    Grid.SetRow(bt, i);
+                    Grid.SetColumn(bt, j);
+                    buttons.Add(bt);
+                    v++;
+                }
+            }
+            buttons.ForEach(bt => grid.Children.Add(bt));
+            MainWind.Height += tbAns.Height + 50;
+
+        }
+
 
         public void Anser(int number)
         {
-            if (number== a)
+            if (number == a)
             {
                 tbAns.Text = ("正解!");
             }
             else
             {
-                if(number>a)
+                if (number > a)
                 {
                     tbAns.Text = ("この数値より少ないです。");
                 }
@@ -48,8 +89,9 @@ namespace NumberGame
 
         private void bt_Click(object sender, RoutedEventArgs e)
         {
-            Anser(int.Parse(((Button) sender).Content.ToString()));
+            Button selecttedButton = (Button)sender;
+            Anser((int)selecttedButton.Content);
         }
-
     }
+
 }
